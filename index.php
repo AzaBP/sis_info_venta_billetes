cc35d32725fefaa19bcae238138d6b839f549ce5
AzaBP <902020@unizar.es>
2026-03-23 19:14:39 +0100

    arreglos modulo ofertas y promociones carrousell inicio aza 23/03/2026 19:12

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 require_once __DIR__ . '/php/auth_helpers.php';
+require_once __DIR__ . '/php/Conexion.php'; 
+
 $usuarioSesion = $_SESSION['usuario'] ?? null;
 if ($usuarioSesion && ($usuarioSesion['tipo_usuario'] ?? '') === 'empleado') {
     header('Location: ' . trainwebRutaPorRol($usuarioSesion));
