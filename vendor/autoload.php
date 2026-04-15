7dd6d9230a71e2f9a3c8cb5df9ef75b6e24b60b9
AzaBP <azadintrescero@gmail.com>
2026-04-16 01:53:53 +0200

    Guardando cambios antes de pull

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -14,9 +14,12 @@ if (PHP_VERSION_ID < 50600) {
             echo $err;
         }
     }
-    throw new RuntimeException($err);
+    trigger_error(
+        $err,
+        E_USER_ERROR
+    );
 }
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitc970f1a266f43a2118de462ce989b839::getLoader();
+return ComposerAutoloaderInit7c7e27d93ea1a67cd25bcaae5c6aae40::getLoader();
